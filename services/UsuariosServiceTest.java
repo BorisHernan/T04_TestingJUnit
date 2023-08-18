f139d7a85bd65fc63c12002ec2375293300f6da6
BorisHernan <boris.medrano@vallegrande.edu.pe>
2023-08-17 23:28:19 -0500

    codigo fuente usado en la exposiciion

---
+++ services/UsuariosServiceTest.java
@@ -0,0 +1,38 @@
+package Sesion11.services;
+
+import Sesion11.entities.Usuario;
+import Sesion11.entities.UsuariosBuilder;
+import Sesion11.repositories.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UsuariosServiceTest {
+
+    @Mock
+    UsuariosDB usuariosDB;//= mock(UsuariosDBFichero.class);
+
+    @InjectMocks
+    UsuariosService usuariosService; //= new UsuariosService();
+
+    Usuario usuario = new UsuariosBuilder("usuarioStub").build();
+
+    @Test
+    void crearUsuario() {
+        when(usuariosDB.insertar(usuario))
+                .thenReturn(true);
+               // .thenThrow(IndexOutOfBoundsException.class);
+
+        boolean resultado = usuariosService.crearUsuario(usuario);
+        assertTrue(resultado);
+    }
+}
